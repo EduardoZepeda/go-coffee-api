4fc0614bdada61f3f916c6125d20ab246c0b53ff
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-06-23 21:50:48 -0500

    Add basic get by Id and full text search

--- api/cafes/cafeList.go
+++ api/cafes/cafeList.go
@@ -1,13 +1,13 @@
 package cafes
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/EduardoZepeda/go-coffee-api/api/types"
 	"github.com/EduardoZepeda/go-coffee-api/web"
+	"github.com/gorilla/mux"
 )
 
 type Shop struct {
@@ -16,8 +16,8 @@ type Shop struct {
 	Address      string      `db:"address" json:"address,omitempty"`
 	Location     types.Point `sql:"type:geometry"`
 	Rating       float32     `db:"rating" json:"rating,omitempty"`
-	CreatedDate  time.Time   `db:"created_date" json:"created_date"`
-	ModifiedDate time.Time   `db:"modified_date" json:"modified_date"`
+	CreatedDate  time.Time   `db:"created_date" json:"created_date,omitempty"`
+	ModifiedDate time.Time   `db:"modified_date" json:"modified_date,omitempty"`
 }
 
 func GetCafes(w http.ResponseWriter, r *http.Request) {
@@ -25,8 +25,8 @@ func GetCafes(w http.ResponseWriter, r *http.Request) {
 	pageStr := r.URL.Query().Get("page")
 	sizeStr := r.URL.Query().Get("size")
 
-	var page = uint64(0)
 	// Default offset value of 10
+	var page = uint64(0)
 	var size = uint64(10)
 
 	if pageStr != "" {
@@ -43,12 +43,69 @@ func GetCafes(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	query := fmt.Sprintf("SELECT id, name, location, address, rating, created_date, modified_date FROM shops_shop ORDER BY name DESC LIMIT %d OFFSET %d;", size, page*size)
-
+	query := "SELECT id, name, location, address, rating, created_date, modified_date FROM shops_shop ORDER BY name DESC LIMIT $1 OFFSET $2;"
 	db, _ := web.ConnectToDB()
 	var cafes []Shop
-	if err := db.SelectContext(r.Context(), &cafes, query); err != nil {
+	if err := db.SelectContext(r.Context(), &cafes, query, size, page*size); err != nil {
 		web.Respond(w, cafes, http.StatusInternalServerError)
+		return
+	}
+	if len(cafes) == 0 {
+		// if query returns nothing return 404 and []
+		web.Respond(w, []int{}, http.StatusNotFound)
+		return
+
+	}
+	web.Respond(w, cafes, http.StatusOK)
+}
+
+func GetCafeById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	db, _ := web.ConnectToDB()
+	var cafes Shop
+	query := "SELECT id, name, location, address, rating, created_date, modified_date FROM shops_shop WHERE id = $1;"
+	if err := db.Get(&cafes, query, params["id"]); err != nil {
+		// If not found {} and 404
+		web.Respond(w, struct{}{}, http.StatusNotFound)
+		return
+	}
+	web.Respond(w, cafes, http.StatusOK)
+}
+
+func SearchCafe(w http.ResponseWriter, r *http.Request) {
+	var err error
+	params := mux.Vars(r)
+	pageStr := r.URL.Query().Get("page")
+	sizeStr := r.URL.Query().Get("size")
+
+	// Default offset value of 5
+	var page = uint64(0)
+	var size = uint64(5)
+
+	if pageStr != "" {
+		page, err = strconv.ParseUint(pageStr, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	if sizeStr != "" {
+		size, err = strconv.ParseUint(sizeStr, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	db, _ := web.ConnectToDB()
+	var cafes []Shop
+	query := "SELECT id, name, location, address, rating, created_date, modified_date FROM shops_shop WHERE to_tsvector(COALESCE(name, '') || COALESCE(address, '')) @@ plainto_tsquery($1) LIMIT $2 OFFSET $3;"
+	if err := db.SelectContext(r.Context(), &cafes, query, params["searchTerm"], size, page*size); err != nil {
+		web.Respond(w, &cafes, http.StatusInternalServerError)
+		return
+	}
+	if len(cafes) == 0 {
+		// if query returns nothing return 404 and []
+		web.Respond(w, []int{}, http.StatusNotFound)
 		return
 	}
 	web.Respond(w, cafes, http.StatusOK)
--- api/handler/main.go
+++ api/handler/main.go
@@ -10,5 +10,7 @@ import (
 func Api(w http.ResponseWriter, r *http.Request) {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/cafes", cafes.GetCafes).Methods(http.MethodGet)
+	router.HandleFunc("/api/cafes/{id}", cafes.GetCafeById).Methods(http.MethodGet)
+	router.HandleFunc("/api/cafes/search/{searchTerm}", cafes.SearchCafe).Methods(http.MethodGet)
 	router.ServeHTTP(w, r)
 }
