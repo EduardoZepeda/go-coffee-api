a64ef875173c30a65428e7f92c91abc22e4a2a2e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-06-27 15:32:02 -0500

    Add blank, null to coffee shop model

--- src/components/CoffeeShopModel.js
+++ src/components/CoffeeShopModel.js
@@ -10,10 +10,10 @@ export default function CoffeeShopModel() {
         { "field": "id", "description": "Coffee ship's unique Id consist of an integer, autoincremental", "type": "int", "null": "", "blank": "", "max length": "" },
         { "field": "name", "description": "Coffee shop's name", "type": "string", "null": "", "blank": "", "max length": 100 },
         { "field": "location", "description": "The coordinates of the coffee shop in the form of tuple: [-123.123456, 123.123456]", "type": "Point", "null": "", "blank": "", "max length": "" },
-        { "field": "address", "description": "Coffee shop's address, number included", "type": "string", "null": "", "max length": "50" },
-        { "field": "rating", "description": "Our main barista rating for that coffee shop. Min 0, Max 5", "type": "Float", "null": "", "max length": "" },
-        { "field": "created_date", "description": "Date when coffee shop was registered", "type": "Datetime", "null": "", "max length": "" },
-        { "field": "modified_date", "description": "Date when coffee shop was last updated", "type": "Datetime", "null": "", "max length": "" }
+        { "field": "address", "description": "Coffee shop's address, number included", "type": "string", "null": true, "blank": true, "max length": "50" },
+        { "field": "rating", "description": "Our main barista rating for that coffee shop. Min 0, Max 5", "type": "Float", "null": true, "blank": true, "max length": "" },
+        { "field": "created_date", "description": "Date when coffee shop was registered", "type": "Datetime", "null": "", "blank": "", "max length": "" },
+        { "field": "modified_date", "description": "Date when coffee shop was last updated", "type": "Datetime", "null": "", "blank": "", "max length": "" }
     ]
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Typography from '@mui/material/Typography';
 import Headings from './Headings';
 import { Link } from '@mui/material';
+import { Box } from '@mui/system';
 
 export default function Home() {
 
@@ -18,6 +19,9 @@ export default function Home() {
                 This API returns information about speciality coffee shops in Guadalajara, Mexico. Please refer to documentation in the sidebar panel for further details. It was created as a personal project to practice go web server capabilities.
             </Typography>
             <Typography sx={{ color: 'text.secondary', maxWidth: '76ch', fontSize: '1.3rem' }}>Try <Link href='/api/v1/cafes'>/api/v1/cafes</Link></Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'center', my: 4 }} >
+                <img alt="logo img" src={"logo192.png"}></img>
+            </Box>
         </>
     )
 }
