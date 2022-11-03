85ee0eeb0d3575063ee939ca65b53a0ddc0d0c07
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-11-02 22:30:51 -0600

    Fix typo in cors allow all middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,7 +9,7 @@ import (
 func CorsAllowAll(app *application.App) func(http http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			r.Header.Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			next.ServeHTTP(w, r)
 		})
 	}
