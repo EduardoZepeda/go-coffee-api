9c1cc7d5fd41042e6184003fd46eb9b5f561e64b
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-11-04 20:32:18 -0600

    Update documentation

--- docs/docs.go
+++ docs/docs.go
@@ -1421,7 +1421,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "go-coffee-api.vercel.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Coffee Shops in Gdl API",
