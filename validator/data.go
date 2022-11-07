39e2aa77f3269f937879e5d779ff0dab64faf7b0
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-11-07 15:44:03 -0600

    Add validation for user's password
    - At least one uppercase char
    - At least one lowercase char
    - At least one digit
    - Greater than 8 chars

--- validator/data.go
+++ validator/data.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"net/mail"
+	"regexp"
 
 	"github.com/EduardoZepeda/go-coffee-api/database"
 	"github.com/EduardoZepeda/go-coffee-api/models"
@@ -15,8 +16,18 @@ func ValidateCoffeeShop(v *Validator, coffeeShop *models.CoffeeShop) {
 
 func ValidateUserSignup(v *Validator, user *models.SignUpRequest) {
 	_, emailError := mail.ParseAddress(user.Email)
+	// golang regexp uses google's re2 syntax, hence look ahead operator is not available ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$
+	hasLower, _ := regexp.MatchString(`[a-z]`, user.Password)
+	hasUpper, _ := regexp.MatchString(`[A-Z]`, user.Password)
+	hasNumber, _ := regexp.MatchString(`[0-9]`, user.Password)
 	v.Validate(emailError == nil, "Email", "Please enter a valid email address")
+	v.Validate(len(user.Email) < 254, "Email", "Email must be shorter than 254 characters")
 	v.Validate(user.Password == user.PasswordConfirmation, "Password confirmation", "Password and password confirmation didn't match")
+	v.Validate(len(user.Password) >= 8, "Password", "Password length must be equal or longer than 8 characters")
+	v.Validate(hasLower, "Password", "Password must contain a lowercase character")
+	v.Validate(hasUpper, "Password", "Password must contain an uppercase character")
+	v.Validate(hasNumber, "Password", "Password must contain a digit")
+
 }
 
 func ValidateUserUpdate(v *Validator, user *models.UpdateUserRequest) {
